a617cd69de34b3e9d26e39224d430fa7a818adb5
sazarcode <cerberusprogrammer@gmail.com>
2023-03-07 18:40:18 +0000

    First UI integration ready.

--- lib/home.dart
+++ lib/home.dart
@@ -12,6 +12,8 @@ class Home extends StatefulWidget {
 class _Home extends State<Home> {
   Activity a = Activity.random();
 
+  bool showActivity = false;
+
   void sync() async {
     Activity activity = await Fetch.getActivity();
     a = activity;
@@ -21,8 +23,65 @@ class _Home extends State<Home> {
   Widget build(BuildContext context) {
     sync();
     return Scaffold(
-      appBar: AppBar(title: const Text('What to do?')),
-      body: Text(a.activity),
+      body: LayoutBuilder(builder: (context, constraints) {
+        return Padding(
+          padding: const EdgeInsets.all(10),
+          child: Card(
+            elevation: 10,
+            child: SizedBox(
+              width: constraints.maxWidth,
+              height: constraints.maxHeight,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'What to do Today?',
+                        style: TextStyle(fontSize: 56),
+                      ),
+                    ),
+                    FilledButton(
+                      onPressed: () {
+                        setState(() {
+                          showActivity = true;
+                        });
+                      },
+                      child: const Text('Discover it!'),
+                    ),
+                    showActivity
+                        ? Card(
+                            elevation: 10,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    a.activity,
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'Price: ${a.price}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'Accessibility: ${a.accessibility * 100}%',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        : Text(''),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
