972fecbd040773af4d3a940d18ceb3219a62ce2e
sazarcode <cerberusprogrammer@gmail.com>
2023-03-10 05:55:31 +0000

    Pending to finish the radio buttons.

--- lib/search.dart
+++ lib/search.dart
@@ -20,75 +20,143 @@ class _Search extends State<StatefulWidget> {
     }
   }
 
+  bool displayFilterType = false;
+  double sliderValue = 1;
+  String radioValue = 'Free';
+
   @override
   Widget build(BuildContext context) {
     typeStrings();
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Padding(
-          padding: const EdgeInsets.all(10),
-          child: Card(
-            elevation: 10,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  const Text('Filter by Type'),
-                  const Divider(),
-                  SizedBox(
-                    height: 40,
-                    child: ListView.builder(
-                      physics: const ScrollPhysics(),
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      itemCount: isPressed.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return FilledButton(
-                          onPressed: isPressed[index]
-                              ? () {
-                                  times[index] = times[index] + 1;
-
-                                  if (times[index] == 1) {
-                                    for (int i = 0; i < isPressed.length; i++) {
-                                      if (i != index) {
-                                        setState(() {
-                                          isPressed[i] = false;
-                                        });
-                                      }
-                                    }
-                                  }
-
-                                  if (times[index] == 2) {
-                                    for (int i = 0; i < isPressed.length; i++) {
-                                      setState(() {
-                                        isPressed[i] = true;
-                                      });
-                                    }
-                                    times[index] = 0;
-                                  }
-                                }
-                              : null,
-                          child: typeButtons[index],
-                        );
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: Card(
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ListTile(
+                      title: const Center(child: Text('Filter by Type')),
+                      onTap: () {
+                        setState(() {
+                          if (displayFilterType) {
+                            displayFilterType = false;
+                          } else {
+                            displayFilterType = true;
+                          }
+                        });
                       },
                     ),
-                  ),
-                  const Divider(),
-                  const Text('Filter by Participants'),
-                  const Divider(),
-                  const Divider(),
-                  const Text('Filter by Price'),
-                  const Divider(),
-                  const Divider(),
-                  const Text('Filter by Accessibility'),
-                  const Divider(),
-                  const Divider(),
-                ],
+                    const Divider(),
+                    displayFilterType
+                        ? SizedBox(
+                            height: 40,
+                            child: ListView.builder(
+                              physics: const ScrollPhysics(),
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              itemCount: isPressed.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return FilledButton(
+                                  onPressed: isPressed[index]
+                                      ? () {
+                                          times[index] = times[index] + 1;
+
+                                          if (times[index] == 1) {
+                                            for (int i = 0;
+                                                i < isPressed.length;
+                                                i++) {
+                                              if (i != index) {
+                                                setState(() {
+                                                  isPressed[i] = false;
+                                                });
+                                              }
+                                            }
+                                          }
+
+                                          if (times[index] == 2) {
+                                            for (int i = 0;
+                                                i < isPressed.length;
+                                                i++) {
+                                              setState(() {
+                                                isPressed[i] = true;
+                                              });
+                                            }
+                                            times[index] = 0;
+                                          }
+                                        }
+                                      : null,
+                                  child: typeButtons[index],
+                                );
+                              },
+                            ),
+                          )
+                        : Center(),
+                    const Divider(),
+                    ListTile(
+                      title:
+                          const Center(child: Text('Filter by Participants')),
+                      onTap: () {},
+                    ),
+                    const Divider(),
+                    Slider(
+                      onChanged: (double value) {
+                        setState(() {
+                          sliderValue = value;
+                        });
+                      },
+                      onChangeEnd: (value) {},
+                      value: sliderValue,
+                      max: 5,
+                      min: 1,
+                      divisions: 5,
+                      label: '${sliderValue.round()}',
+                    ),
+                    const Divider(),
+                    ListTile(
+                      title: const Center(child: Text('Filter by Price')),
+                      onTap: () {},
+                    ),
+                    const Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('Free'),
+                        Radio<String>(
+                            value: 'Free',
+                            groupValue: radioValue,
+                            onChanged: (String? value) {}),
+                        const Text('Paid'),
+                        Radio<String>(
+                            value: 'Free',
+                            groupValue: radioValue,
+                            onChanged: (String? value) {}),
+                      ],
+                    ),
+                    const Divider(),
+                    ListTile(
+                      title:
+                          const Center(child: Text('Filter by Accessibility')),
+                      onTap: () {},
+                    ),
+                    const Divider(),
+                    const Divider(),
+                    FilledButton(onPressed: () {}, child: Text('Search')),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.search),
+      ),
     );
   }
 }
